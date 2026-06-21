6b20669e6871f3c11c5da0dcfe438de696064597
Pavel Vlasenko <psvlasenko@gmail.com>
2026-06-21 17:43:33 +0300

    add icons

---
+++ src/assets/ui-kit/index.ts
@@ -0,0 +1,6 @@
+export const uiKitAssets = {
+  logo: '/src/assets/ui-kit/logo/logo.svg',
+  logoFull: '/src/assets/ui-kit/logo/logo-full.svg',
+  icons16: '/src/assets/ui-kit/icons-16x16/',
+  icons24: '/src/assets/ui-kit/icons-24x24/',
+};
